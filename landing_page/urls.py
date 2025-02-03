e29551b2aa8e75bc807923dba0bae8b79e163869
chrisprod06 <chrisprodromou06@gmail.com>
2025-02-03 21:49:18 +0200

    minor changes

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -7,4 +7,5 @@ app_name = "landing_page"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -10,3 +10,14 @@ def index(request):
     template = "landing_page/index.html"
     context = {}
     return render(request, template, context)
+
+
+def about(request):
+    """
+    This view is used to render the about page
+    :param request:
+    :return:
+    """
+    template = "landing_page/about.html"
+    context = {}
+    return render(request, template, context)
