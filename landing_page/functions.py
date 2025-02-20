f6d3307283fd6fb158bae4f90c31b411cb392f41
chrisprod06 <chrisprodromou06@gmail.com>
2025-02-20 21:37:13 +0200

    job role bug fix

--- landing_page/functions.py
+++ landing_page/functions.py
@@ -16,7 +16,6 @@ def get_team_members_data():
             "full_name": member.full_name,
             "profile_image_url": member.profile_image.url,
             "description": member.description,
-            "job_role_code": member.job_role.code,
             "job_role_name": member.job_role.name.code,
 
         })
