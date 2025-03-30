20155b1cd21004124846102df3c2a83a936c3a08
chrisprod06 <chrisprodromou06@gmail.com>
2025-03-30 16:43:42 +0300

    minor changes

--- client_messages/utils.py
+++ client_messages/utils.py
@@ -1,6 +1,7 @@
-from client_messages.models import ContactUsMessage
+from client_messages.models import ContactUsMessage, VolunteerApplication
 
 
 def client_messages_badge_callback(request):
     pending_client_messages = ContactUsMessage.objects.filter(status=ContactUsMessage.STATUS_PENDING).count()
-    return pending_client_messages
+    pending_volunteer_applications = VolunteerApplication.objects.filter(status=VolunteerApplication.STATUS_PENDING).count()
+    return pending_client_messages + pending_volunteer_applications
--- webtools/urls.py
+++ webtools/urls.py
@@ -6,7 +6,6 @@ from . import views
 app_name = "webtools"
 
 urlpatterns = [
-    path("api/create-mock-data", views.create_mock_data, name="api-create-mock-data"),
     path("api/get-latest-active-announcement", views.get_latest_active_announcement,
          name="api-get-latest-active-announcement"),
 ]
